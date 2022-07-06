51cf87f34453747d3bb3ce54edb86c3e77611b6c
alexisGuerreropc <alexisguerrer86@gmail.com>
2022-07-06 15:05:24 -0500

    cambios 06

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -169,7 +169,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  Math.random(1)
+  
 }
 
 function esPositivo(numero) {
@@ -231,7 +231,8 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+rps = (base * altura )/2
+return rps;
 }
 
 
@@ -239,7 +240,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  let cambi= euro*1.20;
+  return cambi
 }
 
 
