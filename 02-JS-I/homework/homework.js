b8c552e87baed6fdb64227808eb7760b5f81661c
alexisGuerreropc <alexisguerrer86@gmail.com>
2022-07-05 19:00:16 -0500

    retornar  perimitero

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -223,7 +223,8 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  rps = lado **2 
+  return rps;
 }
 
 
